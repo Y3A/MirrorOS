685b4960cd7ae5a0cd64c4ffeffdf5ad51c6615a
y3a <chiefpiectf@gmail.com>
2022-10-10 06:28:27 -0700

    major refactoring of code after 10 month of not touching

--- src/status.h
+++ src/status.h
@@ -3,6 +3,7 @@
 
 #include "types.h"
 
+#define STATUS_SUCCESS 0 /* Operation was successful */
 #define	EPERM        1	/* Operation not permitted */
 #define	ENOENT       2	/* No such file or directory */
 #define	ESRCH        3	/* No such process */
